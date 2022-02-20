3ca376876e89dc405c805f6d6beef90aecee34f9
Rashid Hashim Shaikh <95464203+Rashid-Hashim-Shaikh@users.noreply.github.com>
2022-02-21 03:15:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(
-    <SpeechProvider appId='7856a0c9-68fb-4e8e-a96e-514195ae40c9' language='en-US'>
+    <SpeechProvider appId='' language='en-US'>
         <Provider>
             <App />
         </Provider>
